30ebf2df160c007811910b5f9f7cde3ff631c532
Matheus Portela <matheus.v.portela@gmail.com>
2016-06-05 13:43:37 -0300

    Modify CLI to allow encode/decode.

--- vigenere_cipher.c
+++ vigenere_cipher.c
@@ -9,7 +9,7 @@
 
 void parse_args(int argc, char** argv);
 void print_help();
-void run_vigenere(char* input, char* pass);
+void run_vigenere(char* cmd, char* input, char* pass);
 
 int main(int argc, char** argv) {
     parse_args(argc, argv);
@@ -18,31 +18,46 @@ int main(int argc, char** argv) {
 
 void parse_args(int argc, char** argv) {
     if (argc == 1) {
-        printf("\"pass\" and \"text\" arguments are missing\n");
+        printf("\"cmd\", \"pass\" and \"text\" arguments are missing\n");
         goto error;
     } else if (argc == 2) {
+        printf("\"pass\" and \"text\" arguments are missing\n");
+        goto error;
+    } else if (argc == 3) {
         printf("\"text\" argument is missing\n");
         goto error;
-    } else if (argc > 3) {
+    } else if (argc > 5) {
         printf("Too many arguments\n");
         goto error;
     }
 
-    char* pass = argv[1];
+    char* cmd = argv[1];
+
+    if (strlen(cmd) < 1) {
+        printf("\"cmd\" must have at least one character\n");
+        goto error;
+    }
+
+    char* pass = argv[2];
 
     if (strlen(pass) < 1) {
         printf("\"pass\" must have at least one character\n");
         goto error;
     }
 
-    char* input = argv[2];
+    if ((strcmp(cmd, "encode") != 0) && (strcmp(cmd, "decode") != 0)) {
+        printf("\"cmd\" must be either \"encode\" or \"decode\"");
+        goto error;
+    }
+
+    char* input = argv[3];
 
     if (strlen(input) < 1) {
         printf("\"input\" must have at least one character\n");
         goto error;
     }
 
-    run_vigenere(input, pass);
+    run_vigenere(cmd, input, pass);
     return;
 
 error:
@@ -50,12 +65,21 @@ error:
 }
 
 void print_help() {
-    printf("Usage: vigenere_cipher pass text\n");
+    printf("Usage: vigenere_cipher cmd pass text\n");
+    printf("\nArguments:\n");
+    printf("  cmd: Either \"encode\" or \"decode\"\n");
+    printf("  pass: Password\n");
+    printf("  text: Text to encode/decode\n");
 }
 
-void run_vigenere(char* input, char* pass) {
+void run_vigenere(char* cmd, char* input, char* pass) {
     char* output = malloc(strlen(input) * sizeof(char));
-    vigenere_encode(output, input, pass);
+
+    if (strcmp(cmd, "encode") == 0)
+        vigenere_encode(output, input, pass);
+    else if (strcmp(cmd, "decode") == 0)
+        vigenere_decode(output, input, pass);
+
     printf("%s\n", output);
     free(output);
 }
